9260045062e85126454cd42977930ff84ffe3953
mwyzer <muhammad.wyzer@gmail.com>
2020-07-06 02:41:09 +0700

    Add Friendly ID

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,15 @@ class Course < ApplicationRecord
   def to_s
     title
   end
-
   has_rich_text :description
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  #this is more secure like payment method
+  # friendly_id :generate_slug, use: : slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
 end
